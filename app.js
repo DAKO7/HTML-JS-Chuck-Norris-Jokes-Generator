b2cd4fab826cf6bf63e8ee9c96cba0f17c6d8f55
Opilane TTHK <opilane@tthk.ee>
2019-09-05 12:22:05 +0300

    Chuck Norris Jokes Generator

---
+++ app.js
@@ -0,0 +1,19 @@
+document.querySelector(".get-jokes").addEventListener("click", getJokes);
+
+function getJokes(e){
+    const number = document.querySelector('input[type=number]').value;
+
+    fetch(`http://api.icndb.com/jokes/random/${number}`)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        let output = "";
+        data.value.forEach(joke => {
+            output += `<li>${joke.joke}</li>`;
+        });
+        document.querySelector(".jokes").innerHTML = output;
+    });
+
+    e.preventDefault();
+}
